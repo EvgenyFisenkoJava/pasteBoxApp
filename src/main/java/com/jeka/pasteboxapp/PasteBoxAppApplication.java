12a52c015f2cb513b2180d3b1efe48dc9f167815
EvgenyFisenkoJava <evgeny.f.aircelle@gmail.com>
2023-03-20 21:14:29 +0200

    Initial commit

---
+++ src/main/java/com/jeka/pasteboxapp/PasteBoxAppApplication.java
@@ -0,0 +1,13 @@
+package com.jeka.pasteboxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteBoxAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteBoxAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeka/pasteboxapp/PasteBoxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeka.pasteboxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteBoxAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
